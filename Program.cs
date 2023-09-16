21d5c365ea1396ebf038f692e7406157762fb364
BEN AMEUR <Bechir.BEN-AMEUR.ext@sodexo.com>
2023-09-16 14:30:45 +0200

    BREAKING CHANGE:vvvvv

--- Program.cs
+++ Program.cs
@@ -13,3 +13,9 @@ public class Employeevvv
     // Employee fields, properties, methods and events are inherited
     // New Manager fields, properties, methods and events go here...
 }
+
+public class Employeevvvvv
+{
+    // Employee fields, properties, methods and events are inherited
+    // New Manager fields, properties, methods and events go here...
+}
