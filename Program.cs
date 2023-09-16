f5d2ed05b20be3de9138039b33311c6e885727f3
BEN AMEUR <Bechir.BEN-AMEUR.ext@sodexo.com>
2023-09-16 14:16:19 +0200

    vv

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Employee
+{
+    // Employee fields, properties, methods and events are inherited
+    // New Manager fields, properties, methods and events go here...
+}
+
+
+public class Employeevvv
+{
+    // Employee fields, properties, methods and events are inherited
+    // New Manager fields, properties, methods and events go here...
+}
